3b57d6a699c2cfbdbbb9c57748026b253fd6ee92
Mark Darnell <madarnell@caci.com>
2017-12-14 11:51:01 -0500

    Converted to use spring framework.

--- src/main/java/com/bits/ingest/GetSpaceTracker.java
+++ src/main/java/com/bits/ingest/GetSpaceTracker.java
@@ -2,20 +2,20 @@ package com.bits.ingest;
 
 import com.bits.ingest.spacetracker.JsonResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
+import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GetSpaceTracker {
@@ -29,57 +29,31 @@ public class GetSpaceTracker {
     private String stPassword;
     
     private final String baseURL = "https://www.space-track.org";
-    private final String authPath = "/auth/login";
+    private final String authPath = "/ajaxauth/login";
     private final String query = "/basicspacedata/query/class/satcat/OBJECT_TYPE/PAYLOAD/DECAY/null-val/PERIOD/%3C%3Enull-val/LAUNCH/%3E2013-01-01/orderby/LAUNCH%20desc/format/json/metadata/true";
 
     public JsonResult getData() throws Exception {
         JsonResult jsonResult = null;
         try {
-            CookieManager manager = new CookieManager();
-            manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
-            CookieHandler.setDefault(manager);
+            RestTemplate template = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
-            URL url = new URL(baseURL + authPath);
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("identity", stUsername);
+            map.add("password", stPassword);
+            map.add("query", baseURL + query);
 
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
 
-            String input = "identity=" + stUsername + "&password=" + stPassword;
-
-            log.info("Loggin into Space Tracker");
-            OutputStream os = conn.getOutputStream();
-            os.write(input.getBytes());
-            os.flush();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            while ((output = br.readLine()) != null) {
-                log.info(output);
-            }
-
-            log.info("Executing query and receiving results");
-            url = new URL(baseURL + query);
-            br = new BufferedReader(new InputStreamReader((url.openStream())));
-            StringBuilder jsonString = new StringBuilder();
-            while ((output = br.readLine()) != null) {
-                jsonString.append(output);
-            }
+            log.info("Logging into space-track.org and executing query");
+            ResponseEntity<String> response = template.postForEntity(baseURL + authPath, request, String.class);
             
-            log.info("Converting JSON string to JsonResult object");
-            log.debug("jsonString=" + jsonString.toString());
+            log.info("Converting result into JsonResult object");
             ObjectMapper mapper = new ObjectMapper();
-            jsonResult = mapper.readValue(jsonString.toString(), JsonResult.class);
-
-            log.info("Logging off Space Tracker");
-            url = new URL(baseURL + "/auth/logout");
-            br = new BufferedReader(new InputStreamReader((url.openStream())));
-            while ((output = br.readLine()) != null) {
-                log.info(output);
-            }
-            conn.disconnect();
-
+            jsonResult = mapper.readValue(response.getBody(), JsonResult.class);
+            log.debug("jsonResult = " + jsonResult);
         } catch (Exception e) {
             StringWriter sw = new StringWriter();
             e.printStackTrace(new PrintWriter(sw));
